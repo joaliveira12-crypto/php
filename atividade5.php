97f0b2d2daf37b4bfdd6405bcb58a1fad7931b63
joaliveira12-cripto <joaliveira12@gmail.com>
2026-04-08 09:16:32 -0300

    atividade5.php

---
+++ atividade5.php
@@ -0,0 +1,12 @@
+<?php 
+$num1 = 10;
+$num2 = 8.5;
+$num3 = 7;
+
+$media = ($num1 + $num2 + $num3) /3;
+
+echo "O primeiro numero e: $num1 <br>";
+echo "o segundo numero e: $num2 <br>";
+echo "O terceiro numero e: $num3 <br>";
+echo "A media e: $media";
+?>
